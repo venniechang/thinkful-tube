d4e06c05efdc25bb962d32510e71a84f57b364bb
Vennie Chang <venniechang@yahoo.com>
2018-02-15 09:55:55 -0800

    added function getYoutubeData

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-console.log("hello");
-
-
+const url = "https://www.googleapis.com/youtube/v3/search"
 
 function handleSubmit(){
 	$(".js-search-form").on("submit", function(e){
 		e.preventDefault();
-		console.log("test");
-
-
-
+		const searchTerm = $(".jquery-text").val(); //need to use search term to grab data from youtube
+		getYoutubeData(searchTerm, showResults)
 	})
+}
 
-
-
+function getYoutubeData(search, callback) {
+	const params = {
+		part: "snippet",
+		key: "AIzaSyBoH3ob0S7PoeyFy_OswhXE4hPMTKrDtA8",
+		q: search
+	}
+	$.getJSON(url, params, callback)
 }
 
 handleSubmit();
-//key: AIzaSyBoH3ob0S7PoeyFy_OswhXE4hPMTKrDtA8
