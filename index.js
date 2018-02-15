e676fe9245086075a23fa8335f26f72866b6bf0a
Vennie Chang <venniechang@yahoo.com>
2018-02-15 09:31:59 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+
+
+function handleSubmit(){
+	$(".js-search-form").on("submit", function(e){
+		e.preventDefault();
+		console.log("test");
+
+
+
+	})
+
+
+
+}
+
+handleSubmit();
+//key: AIzaSyBoH3ob0S7PoeyFy_OswhXE4hPMTKrDtA8
