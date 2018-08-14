df814e62be0f2c5aeece106bc732537135078fab
Vennie Chang <venniechang@yahoo.com>
2018-08-13 18:02:40 -0700

    updated local files

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const url = "https://www.googleapis.com/youtube/v3/search"
 
+
 function handleSubmit(){
-	$(".js-search-form").on("submit", function(e){
-		e.preventDefault();
+	$(".js-search-form").on("submit", function(event){
+		event.preventDefault()
 		const searchTerm = $(".jquery-text").val();
-		getYoutubeData(searchTerm, showResults)
+		getYoutubeData(searchTerm, displayYoutubeData);
 	})
 }
 
@@ -14,10 +15,26 @@ function getYoutubeData(search, callback) {
 		key: "AIzaSyBoH3ob0S7PoeyFy_OswhXE4hPMTKrDtA8",
 		q: search
 	}
+
 	$.getJSON(url, params, callback)
 }
 
-function showResults(data){
-	console.log(data);
+function generateHTML(item){
+	console.log(item);
+	return (
+		`<a class="js-result-name" href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank">
+			<h2>
+				${item.snippet.title}
+			</h2>
+			<img src="${item.snippet.thumbnails.medium.url}">
+		</a>`
+	);
 }
-handleSubmit();
+
+function displayYoutubeData(data){
+	console.log(data);
+	const results = data.items.map((item, index) => generateHTML(item));
+	$('.js-search-results').html(results);
+}
+
+$(handleSubmit);
